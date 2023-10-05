685f73eb26b703115edea5e3989433ac8f2dfbe1
ipacy02 <ipacy02@gmail.com>
2023-10-05 12:13:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point of every code
+ * Printf is used for output
+ * Return: Always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
