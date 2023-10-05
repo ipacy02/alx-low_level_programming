7902dbca4379a160623baaa5bc3c9cff4e07cc6a
ipacy02 <ipacy02@gmail.com>
2023-10-05 11:30:45 -0700

    push my code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * Main -Entry point
- * 
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always return 0
  */
-  int main(void)
+int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
 
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
-  }
