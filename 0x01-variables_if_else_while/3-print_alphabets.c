8a8fc8881ab067c40f8208354409e2f7e9c98649
ipacy02 <ipacy02@gmail.com>
2023-10-06 13:25:45 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - we are printing Alphabets.
+ *
+ * Return: Always return 0
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
+
