964ea741f39c29eee1b40889aecf0d6c615c61a8
ipacy02 <ipacy02@gmail.com>
2023-10-10 10:04:31 -0700

    go ahead

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * main -we are making the alphabet
+ *
+ * Return: void
+ */
+int main(void)
+{
+	char = c;
+	for (c = 'a'; c < 'z'; c++)
+	{
+		_putchar(c);
+		_putchar("\n");
+	}
