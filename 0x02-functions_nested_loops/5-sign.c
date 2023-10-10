35d235ed848ab1603b7cb645f7a2d5f7c2fffb32
ipacy02 <ipacy02@gmail.com>
2023-10-10 13:21:49 -0700

    ghasd

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/** _sign(int n) -enter signs
+ * Return: 1
+ */
+int print_sign(int n)
+{
+	n=0;
+	if (n>0)
+	{
+		_putchar('+');
+	}
+	return (1);
+	if (n==0)
+	{
+		_putchar(0);
+	}
+	return (0);
+	if (n<0)
+	{
+		_putchar(0);
+	}
+	return (-1);
+}
