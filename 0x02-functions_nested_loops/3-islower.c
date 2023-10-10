ec3111ca140180a16b6878b5b30c33a79064838a
ipacy02 <ipacy02@gmail.com>
2023-10-10 12:51:55 -0700

    good

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,15 @@
 #include "main.h"
-/**int _islower(int c) -is the entry
- * Return: 1
+
+/**
+ * _islower - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _islower(int c)
 {
-	if(c<=97 && c<=122)
-		{
-			return (1);
-		}
-		return (0);
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
 }
