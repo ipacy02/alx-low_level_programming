da8ab8ebfe1400ef229ece5d1db48300b609acea
ipacy02 <ipacy02@gmail.com>
2023-10-12 10:25:36 -0700

    good

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers -the entry point
+ * Return: 0 success
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 9; c++)
+	{
+		_putchar(c + '0');
+	}
+}
