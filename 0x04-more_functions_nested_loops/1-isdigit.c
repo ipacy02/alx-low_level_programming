49ee5bab34a003404cc6783de10e59ec23b26da2
ipacy02 <ipacy02@gmail.com>
2023-10-12 09:38:43 -0700

    my commit 2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,15 @@
 #include "main.h"
+/**
+ * _isdigit - main we are using
+ * @c: parameter to be checked
+ * Return: 1 otherwise 0
+ */
 
 
-
+int _isdigit(int c)
+{
+if (c == 0 && c < 9)
+	return (1);
+else
+	return (0);
+}
