e025624cae7f61f2a9858517afb6b5ff1870f868
ipacy02 <ipacy02@gmail.com>
2023-10-12 12:24:13 -0700

    push y

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,18 @@
 #include "main.h"
+
 /**
  * print_most_numbers -print that
  * Return: number to not be printed
  */
+
 void print_most_numbers(void)
 {
 	int c;
-	
-	for (c == 0 && c <= 9 && c != 2 && c != 4)
+
+	for (c = 0 ; c < 10 ; c++)
 	{
-		_putchar(c + '0');
-}
-_putchar('\n');
+		if (c != 2 && c != 4)
+			_putchar(c + '0');
+	}
+	_putchar('\n');
 }
