750a343f8a3eef23d5cf43c1743315cb84bc981a
ipacy02 <ipacy02@gmail.com>
2023-10-23 09:23:32 -0700

    jgasdh

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * _strchr - main function
+  *
+  * @s: Function parameter
+  *
+  * @c: Function parameter
+  *
+  * Return: Always o.
+  */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+	if (s[i] == c)
+	return (s + i);
+	}
+	return (0);
+}
+
