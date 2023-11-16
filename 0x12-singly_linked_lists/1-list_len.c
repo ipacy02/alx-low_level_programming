10c546ef032f78f7490246bbadafb457af521f3a
ipacy02 <ipacy02@gmail.com>
2023-11-16 11:32:06 -0800

    push git

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,19 +3,19 @@
 
 /**
  * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list to use
+ * @h: pointer to the list_t list
  *
  * Return: number of elements in h to use
  */
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
+	size_t n = 0;
 
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
 
